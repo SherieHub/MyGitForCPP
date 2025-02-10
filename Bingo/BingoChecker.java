c115b21eb6a2435fcdfe9fd5a6c40291f62e56a9
SherieHub <guadianasherielyn25@gmail.com>
2025-02-11 01:30:50 +0800

    bingo game

---
+++ Bingo/BingoChecker.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+abstract class BingoChecker implements Runnable {
+    protected final BingoCard card;
+    protected final BingoGame game;
+
+    public BingoChecker(BingoCard card, BingoGame game) {
+        this.card = card;
+        this.game = game;
+    }
+}
