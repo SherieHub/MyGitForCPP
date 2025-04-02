85372b6b8e88b1fcebd4e32ab61a959ea6f3e9b1
Austine <austinelomocso@gmail.com>
2025-04-02 22:13:07 +0800

    added bubble, insertion, and selection

---
+++ SortingAlgoritms/InsertionSort.cpp
@@ -0,0 +1,48 @@
+// C++ program for implementation of Insertion Sort
+#include <iostream>
+using namespace std;
+
+/* A utility function to print array of size n */
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+/* Function to sort array using insertion sort */
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        printArray(arr, n);
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+
+        }
+        arr[j + 1] = key;
+
+    }
+}
+
+
+
+// Driver method
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
+
+/* This code is contributed by Hritik Shah. */
